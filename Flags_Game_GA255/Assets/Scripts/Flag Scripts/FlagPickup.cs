1ecc3e4dbb6c91e3f0c8ff29236bc105d8bfdd46
katametz <elizabethmetzger@lcad.edu>
2024-03-30 13:18:38 -0700

    FLAGS FIXED

--- Flags_Game_GA255/Assets/Scripts/Flag Scripts/FlagPickup.cs
+++ Flags_Game_GA255/Assets/Scripts/Flag Scripts/FlagPickup.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class FlagPickup : MonoBehaviour
 {
     private GameObject player;
-    private Inventory inventory;
+    private InventoryManager inventory;
     //public InventoryManager inventoryManager;
 
     // Start is called before the first frame update
     void Start()
     {
-        inventory = GameObject.FindObjectOfType<Inventory>();
+        inventory = GameObject.FindObjectOfType<InventoryManager>();
     }
 
             private void OnTriggerEnter(Collider other)
