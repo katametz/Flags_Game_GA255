2fab5b727a710746c315e606ea29af2c93057abd
katametz <elizabethmetzger@lcad.edu>
2024-05-11 01:15:19 -0700

    FLASGS FIXED

--- Flags_Game_GA255/Assets/Scripts/Interactions/Torch.cs
+++ Flags_Game_GA255/Assets/Scripts/Interactions/Torch.cs
@@ -7,7 +7,7 @@ public class Torch : MonoBehaviour
     public Light torchLight;
 
     private float currentTimer = 0f;
-    public float torchEnableTime = 5f;
+    public float torchEnableTime = 2f;
 
     // Start is called before the first frame update
     void Start()
